dc4847fb21b98887a82885e8498eee4cc9f58048
elisasilvao <elisasilvao@gmail.com>
2023-02-27 17:19:34 -0300

    para entrega

--- src/routes/Navigation/Navigation.jsx
+++ src/routes/Navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 
-import { Outlet , NavLink} from 'react-router-dom';
+import { NavLink} from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
